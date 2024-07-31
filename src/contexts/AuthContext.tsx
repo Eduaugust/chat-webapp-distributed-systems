5429edfd4059b139bd9d4b3710e00a0da8d6aa21
Eduardo Evangelista <eduardo.evangelista2002@gmail.com>
2024-07-31 09:39:36 -0300

    :bookmark: feat: realease v1.0.0

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 
 export interface AuthContextType {
   isAuthenticated: boolean | string;
@@ -25,14 +25,42 @@ interface AuthProviderProps {
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | string>(false);
   const [isServersConnected, setIsServersConnected] = useState<boolean>(false);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
 
   const login = (username: string) => setIsAuthenticated(username);
   const logout = () => setIsAuthenticated(false);
 
-  const setConnect = (value: boolean) => setIsServersConnected(value);
+  const setConnect = (value: boolean) => {
+    console.log(value)
+    if (!value) {
+      // If value is true, start a timer to set it to true after 5 seconds
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+      const id = setTimeout(() =>{console.log('ixi'); setIsServersConnected(false)}, 1000);
+      setTimeoutId(id);
+    } else {
+      // If value is false, immediately set isServersConnected to false
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+        setTimeoutId(null);
+      }
+      console.log('setConnect')
+      setIsServersConnected(true);
+    }
+  };
+
+  // Clean up timeout if the component is unmounted
+  useEffect(() => {
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [timeoutId]);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout, isServersConnected, setConnect}}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout, isServersConnected, setConnect }}>
       {children}
     </AuthContext.Provider>
   );
